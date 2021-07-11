8a9738208f5c37f8936ec5747133f82be1d68a84
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-07-11 23:54:26 +0700

    use operator 'with'. Accept data from the server.

--- tcp-client.py
+++ tcp-client.py
@@ -1,7 +1,10 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 8888))
-message = input('Введите данные: ')
-sock.send(bytes(message, encoding='utf-8'))
-sock.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect(('127.0.0.1', 8888))
+    message = input('Введите данные: ')
+    sock.send(bytes(message, encoding='utf-8'))
+
+    while True: # Заходим в бесконечный цикл и пытаемся принять соединение
+        data = sock.recv(1024)
+        print('Message: ', data.decode('utf-8'))
--- tcp-server.py
+++ tcp-server.py
@@ -1,19 +1,15 @@
 import socket
 from logic import Tracker
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # передаем в конструктор SOCK_STREAM чтобы использовать tcp протакол
-sock.bind(('127.0.0.1', 8888)) # связываемся, резервируем порт
-sock.listen(5) # размер очереди, сколько соединений мы хотим слушать
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # передаем в конструктор SOCK_STREAM чтобы использовать tcp протакол
+    sock.bind(('127.0.0.1', 8888)) # связываемся, резервируем порт
+    sock.listen(5) # размер очереди, сколько соединений мы хотим слушать
 
-while True: # Заходим в бесконечный цикл и пытаемся принять соединение
-    try:
+    while True: # Заходим в бесконечный цикл и пытаемся принять соединение
         client, addr = sock.accept() # accept смотрит в очереди есть ли какие-нибудь клиенты
-    except KeyboardInterrupt:
-        sock.close()
-        break
-    else:
+        client.setblocking(True)
         data = client.recv(1024)
         tracker = Tracker(data=data.decode('utf-8'))
         result = tracker.process_data()
-        client.close()
+        client.send(bytes(result, encoding='utf-8'))
         print('Message: ', result)
