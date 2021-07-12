eb35281e0b4ea5303c84ecc74916b84f8d232169
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-07-12 10:55:55 +0700

    Delete unused code

--- tcp-server.py
+++ tcp-server.py
@@ -1,19 +1,3 @@
-# import socket
-# from logic import Tracker
-#
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # передаем в конструктор SOCK_STREAM чтобы использовать tcp протакол
-#     sock.bind(('127.0.0.1', 8888)) # связываемся, резервируем порт
-#     sock.listen(5) # размер очереди, сколько соединений мы хотим слушать
-#
-#     while True: # Заходим в бесконечный цикл и пытаемся принять соединение
-#         client, addr = sock.accept() # accept смотрит в очереди есть ли какие-нибудь клиенты
-#         client.setblocking(True)
-#         data = client.recv(1024)
-#         tracker = Tracker(data=data.decode('utf-8'))
-#         result = tracker.process_data()
-#         client.send(bytes(result, encoding='utf-8'))
-#         print('Message: ', result)
-
 import socketserver
 from logic import Tracker
 
