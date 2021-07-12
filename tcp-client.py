7c78312d1adb90d4ad7f80bf382fa1a532a4347a
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-07-12 10:49:44 +0700

    changed server structure.

--- tcp-client.py
+++ tcp-client.py
@@ -4,7 +4,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect(('127.0.0.1', 8888))
     message = input('Введите данные: ')
     sock.send(bytes(message, encoding='utf-8'))
-
-    while True: # Заходим в бесконечный цикл и пытаемся принять соединение
-        data = sock.recv(1024)
-        print('Message: ', data.decode('utf-8'))
+    data = sock.recv(1024)
+    print('Message: ', data.decode('utf-8'))
--- tcp-server.py
+++ tcp-server.py
@@ -1,15 +1,37 @@
-import socket
+# import socket
+# from logic import Tracker
+#
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # передаем в конструктор SOCK_STREAM чтобы использовать tcp протакол
+#     sock.bind(('127.0.0.1', 8888)) # связываемся, резервируем порт
+#     sock.listen(5) # размер очереди, сколько соединений мы хотим слушать
+#
+#     while True: # Заходим в бесконечный цикл и пытаемся принять соединение
+#         client, addr = sock.accept() # accept смотрит в очереди есть ли какие-нибудь клиенты
+#         client.setblocking(True)
+#         data = client.recv(1024)
+#         tracker = Tracker(data=data.decode('utf-8'))
+#         result = tracker.process_data()
+#         client.send(bytes(result, encoding='utf-8'))
+#         print('Message: ', result)
+
+import socketserver
 from logic import Tracker
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # передаем в конструктор SOCK_STREAM чтобы использовать tcp протакол
-    sock.bind(('127.0.0.1', 8888)) # связываемся, резервируем порт
-    sock.listen(5) # размер очереди, сколько соединений мы хотим слушать
 
-    while True: # Заходим в бесконечный цикл и пытаемся принять соединение
-        client, addr = sock.accept() # accept смотрит в очереди есть ли какие-нибудь клиенты
-        client.setblocking(True)
-        data = client.recv(1024)
+class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+class TrackingTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024).strip()
         tracker = Tracker(data=data.decode('utf-8'))
         result = tracker.process_data()
-        client.send(bytes(result, encoding='utf-8'))
         print('Message: ', result)
+        self.request.sendall(bytes(result, encoding='utf-8'))
+
+
+if __name__ == '__main__':
+    with ThreadingTCPServer(('127.0.0.1', 8888), TrackingTCPHandler) as server:
+        server.serve_forever()
